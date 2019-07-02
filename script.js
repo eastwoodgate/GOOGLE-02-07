e49f70da700795a5dbdb6d076838806f05177009
eastwoodgate <48026933+eastwoodgate@users.noreply.github.com>
2019-07-02 18:08:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function invertcolors(){
+
+document.body.classList.toggle('invert');
+
+document.getElementsByClassName('wrong').style.color = "red";
+
+}
+
+
+function pagecontrast(){
+
+document.body.classList.toggle('contrast') ;
+
+}
